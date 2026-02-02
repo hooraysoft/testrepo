3b6386e93452b40e3b8d6a6b9f4174c97b5915fc
Predrag Filipovic <predrag.filipovic@hooraysoft.com>
2026-02-02 22:28:03 +0100

    gitignore

---
+++ TinyUrlWebApi/Controllers/Links.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace TinyUrlWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Links : Controller
+    {
+        [HttpPost]
+        public IActionResult CreateLink()
+        {
+            Console.WriteLine("You hit the CreateLink method");
+            return Ok("Link created");
+        }
+    }
+}
--- TinyUrlWebApi/Program.cs
+++ TinyUrlWebApi/Program.cs
@@ -4,6 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,24 +16,26 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+//var summaries = new[]
+//{
+//    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+//};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+//app.MapGet("/weatherforecast", () =>
+//{
+//    var forecast =  Enumerable.Range(1, 5).Select(index =>
+//        new WeatherForecast
+//        (
+//            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//            Random.Shared.Next(-20, 55),
+//            summaries[Random.Shared.Next(summaries.Length)]
+//        ))
+//        .ToArray();
+//    return forecast;
+//})
+//.WithName("GetWeatherForecast");
+
+app.MapControllers();
 
 app.Run();
 
